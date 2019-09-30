9603e6298bcd5c51585bcf3e3183301952e6e553
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-30 01:02:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,7 +87,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday_hash.each do |holiday, item|
       if item.include?("BBQ")
-        holidays << holiday.keys
+        holidays << holiday
         return holidays
       end
     end
