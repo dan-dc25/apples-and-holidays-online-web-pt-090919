f5cfb282347648550e514a116b87b2965bf165fd
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-30 00:55:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,16 +80,15 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 
+
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holidays = []
-  holiday_hash.each do |season, holiday|
-    holiday_hash.each do |holiday, item|
-      if item.include?("BBQ")
-        holidays << holiday.keys
-        return holidays
-      end
+  holiday_hash.map  do |season, holiday|
+    holiday_hash.map do |holiday, item|
+       if item.include?("BBQ")
+         return holiday.keys
+    end
     end
   end
 end
